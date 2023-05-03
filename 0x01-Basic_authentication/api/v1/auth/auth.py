c98af3283070eeefb62dc5e34ca7c624841e2756
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-04 00:46:06 +0100

    route authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method
         """
-        return False
+        if not path:
+            return True
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths or path+'/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header method.
