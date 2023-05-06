d4ef777c18ea15b3e609cf955a36071b7bfabbed
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-06 19:48:39 +0100

    verify user based on email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,11 +52,12 @@ class BasicAuth(Auth):
             user_pwd: str) -> TypeVar('User'):
         """ Returns the `User` instance based on email and passeord.
         """
+        usr = User()
         if not (user_email or isinstance(user_email, str)):
             return None
         if not (user_pwd or isinstance(user_pwd, str)):
             return None
-        user_list = User.search({'email': user_email})
+        user_list = usr.search({'email': user_email})
         if len(user_list) > 0:
             user = user_list[0]
             return user if user.is_valid_password(user_pwd) else None
