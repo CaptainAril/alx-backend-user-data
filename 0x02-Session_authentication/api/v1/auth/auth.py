971edd32fca43e50722a1bf2935a07ee3a9b9f70
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-10 13:24:40 +0100

    Retrieve session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -38,3 +39,11 @@ class Auth:
         """ current user method.
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request.
+        """
+        if not request:
+            return None
+        _my_session_id = getenv('SESSION_NAME')
+        return request.cookies.get(_my_session_id)
