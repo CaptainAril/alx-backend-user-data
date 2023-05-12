96462f59b5bf30a82268bc58683801e1bc3d4159
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-12 08:35:26 +0100

    user registration endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """ Basic Flask app.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -12,6 +14,21 @@ def get():
     """Basic flask route.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """ End point to register a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": "<{}>".format(email),
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registerd"}), 400
 
 
 if __name__ == "__main__":
