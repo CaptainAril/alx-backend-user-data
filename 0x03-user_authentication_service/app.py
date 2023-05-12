a7fe9f90af1cedc6d572ce94b69649b1a5d11c28
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-05-12 23:23:44 +0100

    implemented user-session login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ Basic Flask app.
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -25,10 +25,25 @@ def users():
 
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": "<{}>".format(email),
+        return jsonify({"email": "{}".format(email),
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registerd"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Login function that creates a session, logs in user by session id,
+    and returns session id as cookie.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        return abort(401)
+    sess_id = AUTH.create_session(email)
+    res = jsonify({"email": "{}".format(email), "message": "logged in"})
+    res.set_cookie("session_id", sess_id)
+    return res
 
 
 if __name__ == "__main__":
